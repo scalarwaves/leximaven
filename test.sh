fecdff039f9ef47f1561fee139165f24279a6ec4
Andrew Prentice <scalarwaves@null.net>
2019-01-04 11:42:27 -0500

    ci(ci): fix build

--- test.sh
+++ test.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env bash
 
 # Get definitions for 'catharsis'
-node bin/leximaven.js leximaven wordnik define catharsis
+leximaven wordnik define catharsis
 #Get antonyms for 'noise'
-node bin/leximaven.js leximaven wordnik relate --canon --type antonym noises
+leximaven wordnik relate --canon --type antonym noises
 # Pronounce 'quixotic'
-node bin/leximaven.js leximaven wordnik pronounce quixotic
+leximaven wordnik pronounce quixotic
 # Get etymology for 'special'
-node bin/leximaven.js leximaven wordnik origin special
+leximaven wordnik origin special
 # Get words that sound like 'blue'
-node bin/leximaven.js leximaven datamuse get sl=blue
+leximaven datamuse get sl=blue
 # Get slang/colloquialisms for 'diesel'
-node bin/leximaven.js leximaven urban diesel
+leximaven urban diesel
 # Get a wordmap for 'ubiquity'
-node bin/leximaven.js leximaven wordmap ubiquity
+leximaven wordmap ubiquity
